8c3817f20bc6bbe74142e419441c9f749658aade
Chaitanya Bhardwaj <chaitanya.0498@gmail.com>
2022-09-18 03:58:07 +0200

    create list input

--- bubble sort.py
+++ bubble sort.py
@@ -10,6 +10,13 @@ def bubble_sort(list):
                 list[i], list[i+1] = list[i+1], list[i]
         unsorted_until_index = unsorted_until_index -1
 
-    list[] = input("Type in the list of numbers separated by a comma: ")
-    bubble_sort(list)
-    print(list)
+list = []
+n = int(input("Enter number of elements: "))
+for i in range(0, n):
+    ele = int(input())
+
+    list.append(ele)
+
+# input("Type in the list of numbers separated by a comma: ")
+bubble_sort(list)
+print(l)

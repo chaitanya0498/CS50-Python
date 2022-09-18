d373fdd7057f77bf2faa86485d69d51aa7c66a72
Chaitanya Bhardwaj <chaitanya.0498@gmail.com>
2022-09-18 03:47:33 +0200

    add bubble sort code

---
+++ bubble sort.py
@@ -0,0 +1,15 @@
+def bubble_sort(list):
+    unsorted_until_index = len(list) -1
+    sorted = False
+
+    while not sorted:
+        sorted = True
+        for i in range(unsorted_until_index):
+            if list[i] > list[i+1]:
+                sorted = False
+                list[i], list[i+1] = list[i+1], list[i]
+        unsorted_until_index = unsorted_until_index -1
+
+    list[] = input("Type in the list of numbers separated by a comma: ")
+    bubble_sort(list)
+    print(list)
